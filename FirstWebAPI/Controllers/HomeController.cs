727cc50fdc64a7534e1c1b8860709f11e2c77a90
Melissa <melissakheying@gmail.com>
2021-01-27 14:04:56 -0600

    * .gitignore: Build first Web API using C#
    * FirstWebAPI.sln:
    * Program.cs:
    * Startup.cs:
    * appsettings.json:
    * FirstWebAPI.csproj:
    * WeatherForecast.cs:
    * appsettings.Development.json:
    * HomeController.cs:
    * launchSettings.json:
    * WeatherForecastController.cs:

---
+++ FirstWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FirstWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("HEllo World");
+        }
+
+        [HttpPost]
+        public IActionResult Post(JObject payload)
+        {
+            return Ok(payload);
+        }
+    }
+}
